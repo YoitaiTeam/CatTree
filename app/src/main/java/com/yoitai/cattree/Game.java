7926ca3916bcb5a5a42ebf5c8f0e10e6237ad0d3
yuarak <yuarak@yahoo-corp.jp>
2015-12-11 18:26:07 +0900

    confrict解消

--- app/src/main/java/com/yoitai/cattree/Game.java
+++ app/src/main/java/com/yoitai/cattree/Game.java
@@ -45,31 +45,19 @@ public class Game {
     MainRenderer mMyRenderer;
     SePlayer mSePlayer;
     MediaPlayer mBgmPlayer;
+    Menu mMenu;
 
     Input mInput;
 
     // 内容が変化するゲーム情報
     long mFrameNo;
     Stage mStage;
-<<<<<<< HEAD
-    CAT[] mCAT;
-    Menu mMenu;
-=======
     CatTree mCatTree;
->>>>>>> develop
 
     // コンストラクタ
     public Game() {
         mStage = new Stage();
-<<<<<<< HEAD
-        mCAT = new CAT[5];
-        for (int i = 0; i < 5; i++) {
-            mCAT[i] = new CAT();
-        }
-        mMenu = new Menu();
-=======
         mCatTree = new CatTree();
->>>>>>> develop
     }
 
     // viewの設定
@@ -82,20 +70,9 @@ public class Game {
 
         mStage.setView(_view);
 
-<<<<<<< HEAD
-        for (int i = 0; i < 5; i++) {
-            mCAT[i].setView(_view);
-            mCAT[i].setInput(mInput);
-            mCAT[i].setStage(mStage);
-        }
-
-        mMenu.setView(_view);
-        mMenu.setInput(mInput);
-=======
         mCatTree.setView(_view);
         mCatTree.setInput(mInput);
         mCatTree.setStage(mStage);
->>>>>>> develop
     }
 
     // ゲーム初期化処理(MyRendererからonSurfaceCreated時に実行されます)
@@ -103,21 +80,8 @@ public class Game {
         // 各テクスチャ読み込み
         mMyRenderer.getTexture(TEXNO_BACK).readTexture(mMainActivity, "background.png", 512, 512, 256.0f, 256.0f, 0.0f, 0.0f);
         mMyRenderer.getTexture(TEXNO_CATTREE).readTexture(mMainActivity, "tree.png", 512, 512, 256.0f, 256.0f, 0.0f, 0.0f);
-<<<<<<< HEAD
         mMyRenderer.getTexture(MENU01).readTexture(mMainActivity, "menu01.png", 50, 50, 256.0f, 256.0f, 0.0f, 0.0f);
         mMyRenderer.getTexture(MENU02).readTexture(mMainActivity, "menu02.png", 50, 50, 256.0f, 256.0f, 130.0f, 0.0f);
-        mMyRenderer.getTexture(TEXNO_CHAR0).readTexture(mMainActivity, "cat.png", 256, 256, 100.0f, 128.0f, -100.0f, -128.0f);
-        mMyRenderer.getTexture(TEXNO_CHAR1).readTexture(mMainActivity, "ufo_fire1.png", 173, 138, 84.0f, 57.0f, -84.0f, -57.0f);
-        mMyRenderer.getTexture(TEXNO_CHAR2).readTexture(mMainActivity, "ufo_fire2.png", 173, 138, 84.0f, 57.0f, -84.0f, -57.0f);
-        mMyRenderer.getTexture(TEXNO_ENEMY0).readTexture(mMainActivity, "bird1.png", 86, 79, 44.0f, 40.0f, -44.0f, -40.0f);
-        mMyRenderer.getTexture(TEXNO_ENEMY1).readTexture(mMainActivity, "bird2.png", 90, 79, 44.0f, 40.0f, -44.0f, -40.0f);
-        mMyRenderer.getTexture(TEXNO_BULLET).readTexture(mMainActivity, "bullet.png", 26, 18, 13.0f, 8.0f, -13.0f, 8.0f);
-        mMyRenderer.getTexture(ALBUM01).readTexture(mMainActivity, "album.png", 500, 300, 0.0f, 0.0f, -60.0f, -100.0f);
-        mMyRenderer.getTexture(BTN_CLOSE01).readTexture(mMainActivity, "x.png", 60, 60, 0.0f, 0.0f, 280.0f, -120.0f);
-        mMyRenderer.getTexture(ALBUM02).readTexture(mMainActivity, "album2.png", 500, 300, 0.0f, 0.0f, -60.0f, -100.0f);
-=======
-        mMyRenderer.getTexture(TEXNO_TBLOCK).readTexture(mMainActivity, "tpole.png", 109, 512, 0.0f, 498.0f, 0.0f, -498.0f);
-        mMyRenderer.getTexture(TEXNO_BBLOCK).readTexture(mMainActivity, "bpole.png", 109, 512, 0.0f, 16.0f, 0.0f, -16.0f);
         mMyRenderer.getTexture(TEXNO_CAT0).readTexture(mMainActivity, "cat.png", 70, 105, 35.0f, 52.0f, -35.0f, -52.0f);
         mMyRenderer.getTexture(TEXNO_CAT1).readTexture(mMainActivity, "cat2.png", 70, 105, 35.0f, 52.0f, -35.0f, -52.0f);
         mMyRenderer.getTexture(TEXNO_CAT2).readTexture(mMainActivity, "cat3.png", 72, 128, 36.0f, 0.0f, -36.0f, 0.0f);
@@ -141,6 +105,9 @@ public class Game {
         mMyRenderer.getTexture(TEXNO_ENEMY0).readTexture(mMainActivity, "bird1.png", 86, 79, 44.0f, 40.0f, -44.0f, -40.0f);
         mMyRenderer.getTexture(TEXNO_ENEMY1).readTexture(mMainActivity, "bird2.png", 90, 79, 44.0f, 40.0f, -44.0f, -40.0f);
         mMyRenderer.getTexture(TEXNO_BULLET).readTexture(mMainActivity, "bullet.png", 26, 18, 13.0f, 8.0f, -13.0f, 8.0f);
+        mMyRenderer.getTexture(ALBUM01).readTexture(mMainActivity, "album.png", 500, 300, 0.0f, 0.0f, -60.0f, -100.0f);
+        mMyRenderer.getTexture(BTN_CLOSE01).readTexture(mMainActivity, "x.png", 60, 60, 0.0f, 0.0f, 280.0f, -120.0f);
+        mMyRenderer.getTexture(ALBUM02).readTexture(mMainActivity, "album2.png", 500, 300, 0.0f, 0.0f, -60.0f, -100.0f);
 
         // 各SE読み込み
         mSePlayer.initialize(mMainActivity);
@@ -150,7 +117,6 @@ public class Game {
         // BGM設定
         mBgmPlayer.setLooping(true);
         mBgmPlayer.start();
->>>>>>> develop
     }
 
     // 毎フレーム処理(FPS毎にMainThreadから呼ばれます)
@@ -175,16 +141,7 @@ public class Game {
         // ステージ描画
         mStage.draw(0);
 
-<<<<<<< HEAD
-        // ねこの描画
-        for (int i = 0; i < 5; i++) {
-            mCAT[i].draw();
-        }
-
-        mMenu.draw();
-=======
         // ねこのなる木描画
         mCatTree.draw();
->>>>>>> develop
     }
 }
